23cd8a93474a3769f9fff07cc94ddc49be0f2362
Bahaar Rokhva <rokhvab@gmail.com>
2019-08-13 22:06:46 -0600

    adding initial files

---
+++ assets/javascript/script.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAWOLSPOMm7bDgawNfFbP-JwsGMR7unxFY",
+    authDomain: "train-scheduler-a8441.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a8441.firebaseio.com",
+    projectId: "train-scheduler-a8441",
+    storageBucket: "",
+    messagingSenderId: "397537133133",
+    appId: "1:397537133133:web:162fbe180bb2527e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
