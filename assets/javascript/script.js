95abf4e86fd8d705ff3a92a8fcaffee109e8ab10
Bahaar Rokhva <rokhvab@gmail.com>
2019-08-13 22:33:48 -0600

    firebase is connected and working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,4 +11,28 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  let database = firebase.database();
+
+  $(".btn").on("click", function(event){
+    event.preventDefault();
+    console.log("hi");
+    let name = $("#name").val();
+    let destination = $("#destination").val();
+    let time = $("#first-time").val(); // need to store this as a time in fire base, use moment to convert?
+    let min= $("#min").val();
+    
+    database.ref().push({
+        name: name,
+        role: destination,
+        date: time,
+        months: min,
+        // dateAdded: firebase.database.ServerValue.TIMESTAMP,
+    })
+
+    $("#name").val("");
+    $("#destination").val("");
+    $("#first-time").val("");
+    $("#min").val("");
+})
+
   
