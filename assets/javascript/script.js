0047dd544d61ff3d3500113b2d6a5b1a76d4e08a
Bahaar Rokhva <rokhvab@gmail.com>
2019-08-19 22:55:57 -0600

    cleaned up and commented all the javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,47 +1,42 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyAWOLSPOMm7bDgawNfFbP-JwsGMR7unxFY",
-    authDomain: "train-scheduler-a8441.firebaseapp.com",
-    databaseURL: "https://train-scheduler-a8441.firebaseio.com",
-    projectId: "train-scheduler-a8441",
-    storageBucket: "",
-    messagingSenderId: "397537133133",
-    appId: "1:397537133133:web:162fbe180bb2527e"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAWOLSPOMm7bDgawNfFbP-JwsGMR7unxFY",
+  authDomain: "train-scheduler-a8441.firebaseapp.com",
+  databaseURL: "https://train-scheduler-a8441.firebaseio.com",
+  projectId: "train-scheduler-a8441",
+  storageBucket: "",
+  messagingSenderId: "397537133133",
+  appId: "1:397537133133:web:162fbe180bb2527e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 
-  let database = firebase.database();
-  let loginTime = 0;
+let database = firebase.database();
 
-  $(document).ready(function(){
-    loginTime = moment().format("HH:mm");
-    console.log(loginTime);
-  })
 
-  //on click listener for when submit button is pressed. Grabs info and sends it to Firebase
-  $(".btn").on("click", function(event){
-    //prevents page from reloading constantly
-    event.preventDefault();
-    console.log("hi");
-   
-    //next four lines grab the all the user values from the form
-    let name = $("#name").val();
-    let destination = $("#destination").val();
-    let time = $("#first-time").val();
-    let min = $("#min").val();
+//on click listener for when submit button is pressed. Grabs info and sends it to Firebase
+$(".btn").on("click", function (event) {
+  //prevents page from reloading constantly
+  event.preventDefault();
+  console.log("hi");
 
-  if (name !== "" || destination !== "" || time !== "" || min !== ""){
+  //next four lines grab the all the user values from the form
+  let name = $("#name").val();
+  let destination = $("#destination").val();
+  let time = $("#first-time").val();
+  let min = $("#min").val();
+
+  //stops the user from submitting a form with emoty fields
+  if (name !== "" || destination !== "" || time !== "" || min !== "") {
+
     //store the grabbed values in firebase
     database.ref().push({
-        name: name,
-        destination: destination,
-        time: time,
-        min: min,
-        
+      name: name,
+      destination: destination,
+      time: time,
+      min: min,
     })
-
     //clears values once the submit button has been pressed
     $("#name").val("");
     $("#destination").val("");
@@ -51,52 +46,51 @@
 })
 
 //grabs only the new information in firebase
-database.ref().on("child_added", function(snapshot){
-  console.log(snapshot);
-  
-  
+database.ref().on("child_added", function (snapshot) {
+
   //creates a new row
   let newRow = $("<tr>");
+
   //grabs the value out of firebase and puts in a new table col (next 3 lines)
   let newName = $("<td>").text(snapshot.val().name);
   let newDestination = $("<td>").text(snapshot.val().destination);
   let newMin = $("<td>").text(snapshot.val().min);
 
- 
-  
-
   //grabbing user value from form for first time of train
   let firstTime = snapshot.val().time;
-  console.log(firstTime);
 
   //grabbing user value from form for frequency of train
   let frequency = snapshot.val().min;
-  console.log(frequency);
 
-  //feeding the time format to moment
+  //feeding the time format to moment, and subtracting a year so the first time is always first
   let parse = moment(firstTime, "HH:mm").subtract(1, "years");;
-  console.log(parse);
 
+  //difference in time between the first time and the current time
   let diffTime = moment().diff(moment(parse), "minutes");
-  console.log("DIFFERENCE IN TIME: " + diffTime);
 
+  //getting the remainder of the difference and the frequency
   let timeRemain = diffTime % frequency;
 
+  //subtracting the frequency of each train from the remainder to get MIN UNTIL NEXT TRAIN
   let minUntilNext = frequency - timeRemain;
-  console.log("MINUTES TILL TRAIN: " + minUntilNext);
 
+  //adds minutes until next train
   let nextTrain = moment().add(minUntilNext, "minutes");
-  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
+  //math for figuring and formatting the arrival time
   let newArrTime = moment(nextTrain).format("hh:mm");
-  console.log(newArrTime);
 
+  //creates a new row item for the number of minutes until next train
   let newTrain = $("<td>").text(minUntilNext);
 
+  //creates a new row item for arrival time of next train
   let nextArrival = $("<td>").text(newArrTime);
-   //appends all the values and colomns into the new row
-   newRow.append(newName, newDestination, newMin, nextArrival, newTrain);
-   //and then adds them to the table body
-   $("tbody").append(newRow);
-  
+
+
+  //appends all the values and colomns into the new row
+  newRow.append(newName, newDestination, newMin, nextArrival, newTrain);
+
+  //and then adds them to the table body
+  $("tbody").append(newRow);
+
 });
